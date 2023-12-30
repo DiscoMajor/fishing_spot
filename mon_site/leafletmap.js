da613c31dfb96ae4d091867cf8eae8cc62c1a96e
DiscoMajor <coolstorybrotherz@gmail.com>
2023-12-30 12:30:37 +0100

    Ajout ping map leaftlet, page maitenance done,boutons hover done

--- mon_site/leafletmap.js
+++ mon_site/leafletmap.js
@@ -1,14 +1,23 @@
 const map = L.map("map").setView([43.576358, 1.408894], 13);
 
 const tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: "abcd",
   maxZoom: 20,
 }).addTo(map);
 
-L.marker([43.526107, 1.285239]).addTo(map).bindPopup("Petit Lac De Bidot").openPopup();
-L.marker([43.584188, 1.358444]).addTo(map).bindPopup("Lac du vieux pigeonnier").openPopup();
-L.marker([43.537343, 1.514988]).addTo(map).bindPopup("Lac de Labège").openPopup();
+var redPin = L.icon({
+  iconUrl: "location_leaflet.png",
+
+  iconSize: [50, 50], // size of the icon
+  iconAnchor: [28, 90], // point of the icon which will correspond to marker's location
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([43.526107, 1.285239], { icon: redPin }).addTo(map).bindPopup("Petit Lac De Bidot").openPopup();
+L.marker([43.584188, 1.358444], { icon: redPin }).addTo(map).bindPopup("Lac du vieux Pigeonnier").openPopup();
+L.marker([43.537343, 1.514988], { icon: redPin }).addTo(map).bindPopup("Lac de Labège").openPopup();
 
 /* const tiles = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}", {
   attribution:
