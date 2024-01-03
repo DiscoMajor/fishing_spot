7dddf8c44ec208cc5702626afed1684cb8e7a5cf
Victor <coolstorybrotherz@gmail.com>
2024-01-03 20:46:10 +0100

    Modif carte

--- mon_site/leafletmap.js
+++ mon_site/leafletmap.js
@@ -1,4 +1,5 @@
 const map = L.map("map").setView([43.576358, 1.408894], 13);
+map.setView([43.56578382655103, 1.3970038497246386], 12);
 
 const tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
   attribution:
@@ -9,15 +10,14 @@ const tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager
 
 var redPin = L.icon({
   iconUrl: "location_leaflet.png",
-
   iconSize: [50, 50], // size of the icon
   iconAnchor: [28, 90], // point of the icon which will correspond to marker's location
   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
 });
 
-L.marker([43.526107, 1.285239], { icon: redPin }).addTo(map).bindPopup("Petit Lac De Bidot").openPopup();
-L.marker([43.584188, 1.358444], { icon: redPin }).addTo(map).bindPopup("Lac du vieux Pigeonnier").openPopup();
-L.marker([43.537343, 1.514988], { icon: redPin }).addTo(map).bindPopup("Lac de Labège").openPopup();
+L.marker([43.526107, 1.285239], { icon: redPin }).addTo(map).bindPopup("<strong>Petit Lac De Bidot</strong> <p>Fonsorbes</p>").openPopup();
+L.marker([43.584188, 1.358444], { icon: redPin }).addTo(map).bindPopup("<strong>Lac du vieux Pigeonnier</strong> <p>Tournefeuille</p>").openPopup();
+L.marker([43.537343, 1.514988], { icon: redPin }).addTo(map).bindPopup("<strong>Lac de Labège</strong> <p>Labège</p>").openPopup();
 
 /* const tiles = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}", {
   attribution:
