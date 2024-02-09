609513d8da4d858ca99f1a5c405132240ae66455
Victor <coolstorybrotherz@gmail.com>
2024-02-09 13:37:43 +0100

    Modif markers on the map

--- mon_site/js/leafletmap.js
+++ mon_site/js/leafletmap.js
@@ -1,6 +1,7 @@
-let map = L.map("map").setView([43.576358, 1.408894], 13);
-map.setView([43.56578382655103, 1.3970038497246386], 12);
+// J'initialise ma carte avec coordonnées géographique pour l'afficher sur le site à un endroit précis (Alentours de Toulouse) et je précise que je veux un zoom de 12.
+let map = L.map("map").setView([43.56578382655103, 1.3970038497246386], 12);
 
+// Tuile personnalisée pour ma carte
 let tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -13,8 +14,8 @@ let tiles = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{
 let redPin = L.icon({
     iconUrl: "js/location_leaflet.png",
     iconSize: [40, 40], // size of the icon
-    iconAnchor: [24, 90], // point of the icon which will correspond to marker's location
-    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+    iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -20], // point from which the popup should open relative to the iconAnchor
 });
 
 // Markers on my map + Pop Up with informations
@@ -22,19 +23,19 @@ let redPin = L.icon({
 L.marker([43.526107, 1.285239], { icon: redPin })
     .addTo(map)
     .bindPopup(
-        "<div><img class='imgPopUpLeaftlet' src='./img/lac_bidot.jpg'></div> <div class='textAndButtonInsidePopUpLeaflet'> <h3>Petit Lac De Bidot</h3> <p>Fonsorbes</p> <button id='btnCloseOverlay' onclick='openOverlay1()' class='bouton-pages' type='submit'>En savoir plus</button> </div>"
+        "<div><img class='imgPopUpLeaftlet' src='./img/lac_bidot.jpg'></div> <div class='textAndButtonInsidePopUpLeaflet'> <h3>Petit Lac De Bidot</h3> <p>Fonsorbes</p> <button id='btnCloseOverlay1' onclick='openOverlay1()' class='bouton-pages' type='submit'>En savoir plus</button> </div>"
     );
 
 L.marker([43.584188, 1.358444], { icon: redPin })
     .addTo(map)
     .bindPopup(
-        "<div><img class='imgPopUpLeaftlet' src='./img/lac_vieux_pigeonnier.png'></div> <div class='textAndButtonInsidePopUpLeaflet'> <h3>Lac du vieux Pigeonnier</h3> <p>Tournefeuille</p> <button id='btnCloseOverlay' onclick='openOverlay2()' class='bouton-pages' type='submit'>En savoir plus</button> </div>"
+        "<div><img class='imgPopUpLeaftlet' src='./img/lac_vieux_pigeonnier.png'></div> <div class='textAndButtonInsidePopUpLeaflet'> <h3>Lac du vieux Pigeonnier</h3> <p>Tournefeuille</p> <button id='btnCloseOverlay2' onclick='openOverlay2()' class='bouton-pages' type='submit'>En savoir plus</button> </div>"
     );
 
 L.marker([43.537343, 1.514988], { icon: redPin })
     .addTo(map)
     .bindPopup(
-        "<div><img class='imgPopUpLeaftlet' src='./img/lac_de_labege.png'></div> <div class='textAndButtonInsidePopUpLeaflet'> <h3>Lac de Labège</h3> <p>Labège</p> <button onclick='openOverlay3()' class='bouton-pages' type='submit'>En savoir plus</button> </div>"
+        "<div><img class='imgPopUpLeaftlet' src='./img/lac_de_labege.png'></div> <div class='textAndButtonInsidePopUpLeaflet'> <h3>Lac de Labège</h3> <p>Labège</p> <button id='btnCloseOverlay3' onclick='openOverlay3()' class='bouton-pages' type='submit'>En savoir plus</button> </div>"
     );
 
 //! OVERLAYS
@@ -46,7 +47,7 @@ function openOverlay1() {
     overlay1.style.right = overlay1.style.right === "0%" ? "-100%" : "0%"; // De ce que j'ai compris, je dois récupérer l'animation (Keyframes de mon css) et lui indiquer de la faire. Donc si à -100%  lorsque je clique sur le bouton elle va à 0% et inversement. Merci internet.
 }
 
-//* Same function as before for overlay 2
+//* For the second overlay
 
 function openOverlay2() {
     let overlay2 = document.getElementById("overlay2");
@@ -54,7 +55,7 @@ function openOverlay2() {
     overlay2.style.right = overlay2.style.right === "0%" ? "-100%" : "0%";
 }
 
-//* Same function as before for the third overlay
+//* For the third overlay
 
 function openOverlay3() {
     let overlay3 = document.getElementById("overlay3");
@@ -62,18 +63,23 @@ function openOverlay3() {
     overlay3.style.right = overlay3.style.right === "0%" ? "-100%" : "0%";
 }
 
-//! Function to close my overlay when i click outside the overlay (NOT WORKING FOR THE MOMENT)
+//! Version "simplifiée sans redondance" : je créé une fonction pour tous mes overlays en donnant un parametre à ma fonction et mes instructions
+// function openOverlay(overlayIDs) {
+//     let overlay = document.getElementById(overlayIDs);
+//     overlay.classList.toggle("hidden");
+//     overlay.style.right = overlay.style.right === "0%" ? "-100%" : "0%";
+// }
+//! Je créé une fonction unique pour chaque overlay en utilisant ma fonction générale "OpenOverlay" et je lui spécifie quel overlay ouvrir.
+// function openOverlay1() {
+//     openOverlay("overlay1");
+// }
+//! J'assigne ensuite mes fonctions aux bontons correspondants de ma carte.
+// function openOverlay2() {
+//     openOverlay("overlay2");
+// }
 
-document.addEventListener("click", function (event) {
-    // let closeoverlay = document.getElementById("overlay1"); // mes overlays ici ??
-    let closeoverlay = document.querySelectorAll("#overlay1, #overlay2"); // mes overlay 1 et 2 dans mon HTML
-    let overlayBtn = document.getElementById("btnCloseOverlay"); // mes boutons
-});
+// function openOverlay3() {
+//     openOverlay("overlay3");
+// }
 
-
-
-//! (je pourrais maybe utiliser query selector all et mes IDS overlay123 pour faire une seule fonction ?
-// ! Du style : document.querySelectorAll("#overlac1, #overlaylac2") ect ect et idem pour fermer l'overlay ?
-
-//! Maybe i can do a loop or a boolean for my 3 functions. To avoid repetitions of the same function but with a different name FALSE OR TRUE
-//! Alors il me faut un addlistener
+//TODO Fonction pour fermer les overlays. Bolean ?
