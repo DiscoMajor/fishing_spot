7f0e48e269c75ff2637fb3b9785f8a65a91425f7
Victor <coolstorybrotherz@gmail.com>
2024-02-18 15:18:46 +0100

    Minor changes on my scss / js

--- mon_site/js/leafletmap.js
+++ mon_site/js/leafletmap.js
@@ -36,7 +36,7 @@ L.marker([43.584188, 1.358444], { icon: redPin })
         `<div><img class='imgPopUpLeaftlet' src='./img/lac_vieux_pigeonnier.png'></div>
         <div class='textPopUpLeaflet'> <h3>Lac du vieux Pigeonnier</h3> 
         <p>📍 Tournefeuille</p> 
-        <button id='btnCloseOverlay2' onclick='openOverlay2()' class='bouton-pages' type='submit'>En savoir plus</button> </div>`
+        <button onclick='openOverlay2()' class='bouton-pages' type='submit'>En savoir plus</button> </div>`
     );
 
 L.marker([43.537343, 1.514988], { icon: redPin })
@@ -45,101 +45,48 @@ L.marker([43.537343, 1.514988], { icon: redPin })
         `<div><img class='imgPopUpLeaftlet' src='./img/lac_de_labege.png'></div> 
         <div class='textPopUpLeaflet'> <h3>Lac de Labège</h3>
         <p>📍 Labège</p> 
-        <button id='btnCloseOverlay3' onclick='openOverlay3()' class='bouton-pages' type='submit'>En savoir plus</button> </div>`
+        <button onclick='openOverlay3()' class='bouton-pages' type='submit'>En savoir plus</button> </div>`
     );
 
 //! OVERLAYS
 
-//* Fonction qui enlève mon texte caché en HTML pour l'afficher par la droite avec une transition :
+// Je crée une fonction générale pour mes overlays en donnant un parametre et  mes instructions
+
+// Je veux que ma function récupère les IDS de mes overlays côté HTML
+function openOverlay(overlayIDs) {
+    let overlay = document.getElementById(overlayIDs); // IDs overlay dans mon HTML
+    overlay.classList.toggle("hidden"); // Bouton ON/OFF de ma classe hidden
+    overlay.style.right = overlay.style.right === "0%" ? "-100%" : "0%"; // J'ajoute la transition fluide de mon css à ma fonction
+}
+
+// Je créé une fonction unique pour chaque overlay en utilisant ma fonction générale "OpenOverlay" et je lui spécifie quel overlay ouvrir.
+// Je récupère l'ID de mon overlay et "onclick", elle s'active.
 function openOverlay1() {
-    let overlay1 = document.getElementById("overlay1"); // Je récupère l'ID de ma div dans mon HTML
-    overlay1.classList.toggle("hidden"); // Je bascule (comme un bouton on/off d'une lumière) le "display: none;" de ma class .hidden situé dans mon html (donc mon texte est visible mais toujours sur la droite de mon écran à -100%)
-    overlay1.style.right = overlay1.style.right === "0%" ? "-100%" : "0%"; // Je récupère l'animation (Keyframes de mon css) et lui indiquer de la faire. Donc si à -100%  lorsque je clique sur le bouton elle va à 0% et inversement. Merci internet.
+    openOverlay("overlay1");
 }
 
-//* For the second overlay
-
+//! Idem pour l'overlay deux, j'applique mes instructions de ma fonction Openoverlay.
 function openOverlay2() {
-    overlay2 = document.getElementById("overlay2");
-    overlay2.classList.toggle("hidden");
-    overlay2.style.right = overlay2.style.right === "0%" ? "-100%" : "0%";
+    openOverlay("overlay2");
 }
-
-//* For the third overlay
 
 function openOverlay3() {
-    overlay3 = document.getElementById("overlay3");
-    overlay3.classList.remove("hidden");
-    overlay3.style.right = overlay3.style.right === "0%" ? "-100%" : "0%";
+    openOverlay("overlay3");
 }
 
-//! Version "simplifiée sans redondance" : je créé une fonction pour tous mes overlays en donnant un parametre à ma fonction et mes instructions
-// function openOverlay(overlayIDs) {
-//     let overlay = document.getElementById(overlayIDs);
-//     overlay.classList.toggle("hidden");
-//     overlay.style.right = overlay.style.right === "0%" ? "-100%" : "0%";
-// }
-//! Je créé une fonction unique pour chaque overlay en utilisant ma fonction générale "OpenOverlay" et je lui spécifie quel overlay ouvrir.
-// function openOverlay1() {
-//     openOverlay("overlay1");
-// }
-//! J'assigne ensuite mes fonctions aux bontons correspondants de ma carte.
-// function openOverlay2() {
-//     openOverlay("overlay2");
-// }
+// Je crée une fonction pour tous mes overlays en donnant deux parametres (Un qui va récuperer les ids des boutons, pour l'id des overlays) et mes instructions
+// Ici je veux que ma function récupère les IDS de mes overlays
+function closeOverlay(closeBtnSelector, overlaySelector) {
+    const closeOverlay = document.querySelector(closeBtnSelector); // Premier parametre => les ids de mes boutons
+    const overlay = document.querySelector(overlaySelector); // Deuxieme parametre => les ids de mes overlay )
 
-// function openOverlay3() {
-//     openOverlay("overlay3");
-// }
-
-//TODO Fonction pour fermer les overlays. ?
-//! Marche mais beaucoup de redondance...
-
-function closeOverlaysBtn1() {
-    closeOverlay1 = document.querySelector("#close-overlay1");
-    closeOverlay1.addEventListener("click", (event) => {
-        overlay1.classList.add("hidden");
-        overlay1.style.right = overlay1.style.right === "0%" ? "-100%" : "0%";
+    // Je veux qu'au clic du bouton de fermeture, on ajoute à mes overlay la classe hidden
+    closeOverlay.addEventListener("click", () => {
+        overlay.classList.add("hidden");
+        overlay.style.right = overlay.style.right === "0%" ? "-100%" : "0%";
     });
 }
-closeOverlaysBtn1("overlay1");
-
-function closeOverlaysBtn2() {
-    closeOverlay2 = document.querySelector("#close-overlay2");
-    closeOverlay2.addEventListener("click", (event) => {
-        overlay2.classList.add("hidden");
-        overlay2.style.right = overlay2.style.right === "0%" ? "-100%" : "0%";
-    });
-}
-closeOverlaysBtn2("overlay2");
-
-function closeOverlaysBtn3() {
-    closeOverlay3 = document.querySelector("#close-overlay3");
-    closeOverlay3.addEventListener("click", (event) => {
-        overlay3.classList.add("hidden");
-        overlay3.style.right = overlay3.style.right === "0%" ? "-100%" : "0%";
-    });
-}
-closeOverlaysBtn3("overlay3");
-
-//! Trying to create the same function and avoid redundancy.. not working ATM
-
-//TODO ADD A CONSOLE LOG TO SEE WHAT IS GOING ON (maybe échanger les valeurs du style.right ou un if else... ? A L'AIDE)
-
-// function OverlaysClosingBtns(btnIds) {
-//     closingBtns = document.getElementById(btnIds);
-//     closingBtns.classList.toggle("hidden");
-//     closingBtns.style.right = closingBtns.style.right === "0%" ? "-100%" : "0%";
-// }
-
-// function overlayBtn1() {
-//     OverlaysClosingBtns("#close-overlay1");
-// }
-
-// function overlayBtn2() {
-//     OverlaysClosingBtns("#close-overlay2");
-// }
-
-// function overlayBtn3() {
-//     OverlaysClosingBtns("#close-overlay3");
-// }
+// j'appel ma function et je selectionne le bouton de l'overlay et l'ID de la div que je veux remettre en hidden.
+closeOverlay("#close-overlay1", "#overlay1");
+closeOverlay("#close-overlay2", "#overlay2");
+closeOverlay("#close-overlay3", "#overlay3");
